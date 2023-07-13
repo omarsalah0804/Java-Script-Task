9ee0bf0f7786df3d8a5c912f618cf064b57ee840
omar salah <135585082+omarsalah0804@users.noreply.github.com>
2023-07-13 11:23:54 +0300

    task4

---
+++ all-task/TEST/main.js
@@ -0,0 +1,11 @@
+// lower case
+
+let text = "GROUP 5!";
+let result = text.toLowerCase();
+console.log(result);
+
+// // upper case
+
+// let text = "group 5!";
+// let result = text.toUpperCase();
+// console.log(result);
