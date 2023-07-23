c786b39d1c3cca35767fd190320cb4b6ab27a0b2
omar salah <135585082+omarsalah0804@users.noreply.github.com>
2023-07-23 17:58:32 +0300

    h

---
+++ PROGCT/task_2.js
@@ -0,0 +1,43 @@
+let signInBtn=document.getElementById("signInId");
+let emailField=document.getElementById("email");
+let passwordField=document.getElementById("password");
+
+//email field
+emailField.addEventListener("focus",function(){
+    document.getElementById("emailP").style.display="none";
+})
+
+
+//password field
+passwordField.addEventListener("focus",function(){
+    document.getElementById("passwordP").style.display="none";
+})
+
+//login button field
+signInBtn.addEventListener("click",function(event){
+    event.preventDefault();
+    let users = JSON.parse(localStorage.getItem("users")) || [];
+    let i=0;
+    for(;i<users.length;i++){
+        if(users[i]["email"]==emailField.value && users[i]["password"]==passwordField.value){
+            alert("Welcome "+ users[i]["firstName"] + "!!");
+            document.getElementById("emailP").style.display="none";
+            document.getElementById("passwordP").style.display="none";
+            localStorage.setItem("userName", JSON.stringify(users[i]["firstName"]));
+            window.location.href=""
+        ;}
+        if(users[i]["email"]==emailField.value && users[i]["password"]!=passwordField.value){
+            document.getElementById("emailP").style.display="none";
+            document.getElementById("passwordP").style.display="block";
+            document.getElementById("passwordP").classListadd("invalid");
+            console.log("error pass");
+            break;}
+    }
+    if(i==users.length){
+        document.getElementById("emailP").style.display="block";
+        document.getElementById("emailP").classListadd("invalid");
+        document.getElementById("passwordP").style.display="none";
+        console.log("error email");
+    }
+    
+})
